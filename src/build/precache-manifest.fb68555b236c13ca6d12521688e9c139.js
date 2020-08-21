aa9f4c76a566a3cb5f0be0bdd97fc4e902847751
Shaun Wassell <shaun@trove.ai>
2020-08-21 16:56:19 -0400

    First commit

---
+++ src/build/precache-manifest.fb68555b236c13ca6d12521688e9c139.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b10ddb39e88539483b188e940b2fc59",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9f81a999d874e26d85b",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "07375ef8443504488f91",
+    "url": "/static/js/2.97f84c92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.97f84c92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9f81a999d874e26d85b",
+    "url": "/static/js/main.e565fa82.chunk.js"
+  },
+  {
+    "revision": "446aefb0fac08d8e1aa8",
+    "url": "/static/js/runtime-main.4c311310.js"
+  }
+]);
---
+++ src/server.js
@@ -0,0 +1,73 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { MongoClient } from 'mongodb';
+import path from 'path';
+import { withDB } from './db';
+
+const articlesInfo = {
+    'learn-react': { upvotes: 0, comments: [] },
+    'learn-node': { upvotes: 0, comments: [] },
+    'my-thoughts-on-resumes': { upvotes: 0, comments: [] },
+};
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/build')));
+app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    await withDB(async db => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles')
+            .findOne({ name: articleName });
+
+        if (articleInfo) {
+            res.status(200).json(articleInfo);
+        } else {
+            res.status(404).send("Article not found!");
+        }
+    });
+});
+
+app.post('/api/articles/:name/upvote', async (req, res) => {
+    withDB(async db => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles')
+            .findOne({ name: articleName });
+
+        if (!articleInfo) return res.status(404).send("Article not found!");
+
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': { upvotes: articleInfo.upvotes + 1 }
+        });
+        const updatedArticleInfo = await db.collection('articles')
+            .findOne({ name: articleName });
+        
+        res.status(200).json(updatedArticleInfo);
+    });
+});
+
+app.post('/api/articles/:name/comment', async (req, res) => {
+    withDB(async db => {
+        const articleName = req.params.name;
+        const newComment = req.body.comment;
+        const articleInfo = await db.collection('articles')
+            .findOne({ name: articleName });
+
+        if (!articleInfo) return res.status(404).send("Article not found!");
+
+        await db.collection('articles').updateOne({ name: articleName }, {
+            '$set': { comments: articleInfo.comments.concat(newComment) }
+        });
+        const updatedArticleInfo = await db.collection('articles')
+            .findOne({ name: articleName });
+        
+        res.status(200).json(updatedArticleInfo);
+    });
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/build/index.html'));
+});
+
+app.listen(8000, () => console.log("Server is listening on port 8000"));
